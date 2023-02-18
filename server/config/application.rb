9ca9dbe79650bbb50df729e6f35048d61f669b99
KubaKonieczny <72551104+KubaKonieczny@users.noreply.github.com>
2023-02-18 17:54:59 +0100

    safer bd conncection

--- server/config/application.rb
+++ server/config/application.rb
@@ -18,11 +18,20 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
+
 module Server
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

76563f750ba87ef0ca01cc1d638a15ee297ba111
Maja Świerk <115174681+manna8@users.noreply.github.com>
2023-03-20 14:34:10 +0100

    Delete client directory

--- client/src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import config from "../config/config";
-
-export default axios.create({
-    baseURL: config.apiURL,
-    headers: {
-        "Content-type": "application/json"
-    }
-});
